a03811cfc2fec0699b225a265deba0c0ccc7d29c
yesenia180405 <yes.ale1804@gmail.com>
2026-03-23 10:51:38 +0100

    chore(nuxt.config.ts): configuration for tailwind CSS

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav>
+<nav class="bg-[]">
   
 </nav>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  },
   devtools: { enabled: true }
 })
