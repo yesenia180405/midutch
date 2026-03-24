5e073fb050f5f0eeb7edf4f30b1b4a0652cf84b2
yesenia180405 <yes.ale1804@gmail.com>
2026-03-24 12:03:57 +0100

    feat(app/components/AppCardStream): implementation the cards of stream

--- app/components/AppCardStream.vue
+++ app/components/AppCardStream.vue
@@ -1,54 +1,27 @@
 <script setup>
-const streams = [
-  {
-    id: "1",
-    user_name: "Miduncy",
-    game_name: "League of Legends",
-    title: "¡Ranked hasta Challenger o me rayo el pelo!",
-    viewer_count: 85420,
-    thumbnail_url: "/img/stream.jpg",
-    profile_image_url: "https://placehold.co/50x50",
-    language: "es",
-    is_mature: false
-  },
-  {
-    id: "2",
-    user_name: "Miduplay",
-    game_name: "Minecraft",
-    title: "Supervivencia día 1 - No me ayuda nadie",
-    viewer_count: 62100,
-    thumbnail_url: "/img/stream.jpg",
-    profile_image_url: "https://placehold.co/50x50",
-    language: "es",
-    is_mature: false
-  },
-  {
-    id: "3",
-    user_name: "Midunxocas",
-    game_name: "Fortnite",
-    title: "Torneo con suscriptores - ¡Premios!",
-    viewer_count: 41300,
-    thumbnail_url: "/img/stream.jpg",
-    profile_image_url: "#",
-    language: "es",
-    is_mature: false
-  }
-]
+const props = defineProps({
+  id: String,
+  user_name: String,
+  game_name: String,
+  title: String,
+  viewer_count: Number,
+  thumbnail_url: String,
+  profile_image_url: String,
+  language: String,
+  is_mature: Boolean
+})
+
 </script>
 <template>
-<section class="text-white  md:grid md:grid-cols-3 gap-14 py-6">
-  <div v-for="stream in streams" :key="stream.id">
-<img :src="stream.thumbnail_url" alt="Stream" class="w-full"/>
-
-<section class="flex gap-3 my-5">
-  <img src="/img/streamer.jpg" alt="streamer" class="h-10 w-10 rounded-full">
-  <div class="">
-    <p class="font-semibold">{{ stream.title }}</p>
-    <p class="text-gray-300">{{ stream.user_name }}</p>
-    <p class="text-gray-300">{{ stream.game_name }}</p>
-  </div>
-</section>
-
-  </div>
-</section>
+  <section>
+      <img :src="props.thumbnail_url" alt="Stream" class="w-full" />
+      <section class="flex gap-3 my-5">
+        <img src="/img/streamer.jpg" alt="streamer" class="h-10 w-10 rounded-full">
+        <div>
+          <p class="font-semibold">{{ props.title }}</p>
+          <p class="text-gray-300">{{ props.user_name }}</p>
+          <p class="text-gray-300">{{ props.game_name }}</p>
+        </div>
+      </section>
+  </section>
 </template>
--- app/components/AppStreams.vue
+++ app/components/AppStreams.vue
@@ -1,11 +1,96 @@
+<script setup>
+const streams = [
+  {
+    id: "1",
+    user_name: "Miduncy",
+    game_name: "League of Legends",
+    title: "¡Ranked hasta Challenger o me rayo el pelo!",
+    viewer_count: 85420,
+    thumbnail_url: "/img/stream.jpg",
+    profile_image_url: "https://placehold.co/50x50",
+    language: "es",
+    is_mature: false
+  },
+  {
+    id: "2",
+    user_name: "Miduplay",
+    game_name: "Minecraft",
+    title: "Supervivencia día 1 - No me ayuda nadie",
+    viewer_count: 62100,
+    thumbnail_url: "/img/stream.jpg",
+    profile_image_url: "https://placehold.co/50x50",
+    language: "es",
+    is_mature: false
+  },
+  {
+    id: "3",
+    user_name: "Midunxocas",
+    game_name: "Fortnite",
+    title: "Torneo con suscriptores - ¡Premios!",
+    viewer_count: 41300,
+    thumbnail_url: "/img/stream.jpg",
+    profile_image_url: "#",
+    language: "es",
+    is_mature: false
+  },
+  {
+    id: "4",
+    user_name: "Miduncy",
+    game_name: "League of Legends",
+    title: "¡Ranked hasta Challenger o me rayo el pelo!",
+    viewer_count: 85420,
+    thumbnail_url: "/img/stream2.jpg",
+    profile_image_url: "https://placehold.co/50x50",
+    language: "es",
+    is_mature: false
+  },
+  {
+    id: "5",
+    user_name: "Miduplay",
+    game_name: "Minecraft",
+    title: "Supervivencia día 1 - No me ayuda nadie",
+    viewer_count: 62100,
+    thumbnail_url: "/img/stream2.jpg",
+    profile_image_url: "https://placehold.co/50x50",
+    language: "es",
+    is_mature: false
+  },
+  {
+    id: "6 ",
+    user_name: "Midunxocas",
+    game_name: "Fortnite",
+    title: "Torneo con suscriptores - ¡Premios!",
+    viewer_count: 41300,
+    thumbnail_url: "/img/stream2.jpg",
+    profile_image_url: "#",
+    language: "es",
+    is_mature: false
+  }
+]
+
+const showMore = ref(false);
+</script>
 <template>
-  <section class="bg-base min-h-[94.5vh]">
-  
-    <AppCardStream/>
-    <AppCardStream/>
-    <AppCardStream/>
-    <AppCardStream/>
-    <AppCardStream/>
-    <AppCardStream/>
+  <section class="bg-base min-h-[94.5vh] text-white">
+    <h2 class="pt-5 pl-2 font-semibold text-lg"> <span class="text-main-text">Live channels</span> we think you'll like
+    </h2>
+
+<section class="md:grid md:grid-cols-3 gap-14 py-6 pr-7">
+  <div v-for="stream in showMore ? streams : streams.slice(0, 3)" :key="stream.id" >
+    <AppCardStream :id="stream.id"
+    :user_name="streams.user_name"
+    :game_name="stream.game_name"
+    :title="stream.title"
+    :viewer_count="stream.viewer_count" 
+    :thumbnail_url="stream.thumbnail_url" 
+    :profile_image_url= "stream.profile_image_url",
+    :language="stream.profile_image_url"/>
+  </div>
+</section>
+
+    <button @click="showMore = !showMore" class="flex gap-3 items-center justify-center">
+      {{ showMore ? "Show less" : "Show more" }}
+      <img src="/img/down.svg" alt="icon" class="invert">
+    </button>
   </section>
 </template>
