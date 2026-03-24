89cebc93d8f81e89c77d86900768f4ec194f20c5
yesenia180405 <yes.ale1804@gmail.com>
2026-03-24 12:46:10 +0100

    feat(app/components AppCategory.vue): enter test data

---
+++ app/components/AppCardCategory.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props= defineProps({
+   id: String,
+    name: String,
+    box_art_url: String,
+    viewer_count: Number,
+    tags: Array
+})
+</script>
+<template>
+  
+</template>
