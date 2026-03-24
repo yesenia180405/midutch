a20bbdb963e64a218ff3adbf4baec0fb1fbfc3dc
yesenia180405 <yes.ale1804@gmail.com>
2026-03-24 10:18:18 +0100

    fix(app/components/AppRecommendations): the side section has been given functionality

--- app/components/AppRecommendations.vue
+++ app/components/AppRecommendations.vue
@@ -8,15 +8,19 @@ const streamers = [
   { name: "Miduxeneize", theme: "Furvo", viewers: "1.5K", img: "/img/streamer.jpg" },
   { name: "Miduelcapo", theme: "JavaScript World", viewers: "8.3K", img: "/img/streamer.jpg" },
 ]
-const open = ref(false);
+const close = ref(false);
 </script>
 <template>
-  <aside class="bg-base min-h-[93vh] relative pt-10" :class="open ? 'w-[5vw]' : 'w-[18vw]'">
-    <img src="/img/collapse.svg" alt="Collapse icon" class="invert absolute right-5" :class="open ? 'rotate-180' : ''" @click="open =!open" />
-    <h2 class="text-white pl-5 font-medium mb-5">RECOMMENDED CHANELS</h2>
+  <aside class="bg-base min-h-[93vh] relative pt-10" :class="close ? 'w-[5vw]' : 'w-[18vw]'">
 
-    <section class="flex flex-col text-white items relative">
-      <div v-for="streamer in streamers" class="flex gap-3 my-3">
+    <div class="flex justify-around items-center mb-5">
+      <h2 v-if="!close" class="text-white  font-medium ">RECOMMENDED CHANELS</h2>
+      <img src="/img/collapse.svg" alt="Collapse icon" class="invert flex " :class="close ? 'rotate-180 invert' : ''"
+      @click="close = !close" />
+    </div>
+      
+      <section class="flex flex-col text-white items relative">
+        <div v-if="!close" v-for="streamer in streamers" class="flex gap-3 my-3">
         <NuxtImg :src="streamer.img" alt="Foto Perfil" width="40" height="40"
           class="border rounded-full border-base ml-5" />
         <div>
@@ -25,6 +29,11 @@ const open = ref(false);
         </div>
         <span class="absolute right-8">🔴 {{ streamer.viewers }}</span>
       </div>
+
+      <div v-else v-for="streamer in streamers" class="flex gap-3 my-3">
+          <NuxtImg :src="streamer.img" alt="Foto Perfil" width="40" height="40"
+            class=" border rounded-full border-base ml-5" />
+        </div>
     </section>
   </aside>
 </template>
