c52340be82681eb878df74cabfa4ff1424fbbb0e
yesenia180405 <yes.ale1804@gmail.com>
2026-03-24 09:53:44 +0100

    fix(app/components/AppRecommendations): the icon has been given functionality

--- app/components/AppRecommendations.vue
+++ app/components/AppRecommendations.vue
@@ -8,15 +8,17 @@ const streamers = [
   { name: "Miduxeneize", theme: "Furvo", viewers: "1.5K", img: "/img/streamer.jpg" },
   { name: "Miduelcapo", theme: "JavaScript World", viewers: "8.3K", img: "/img/streamer.jpg" },
 ]
+const open = ref(false);
 </script>
 <template>
-  <aside class="bg-base min-h-[93vh] w-[18vw] relative pt-10 ">
-    <ArrowLeftEndOnRectangleIcon class="absolute w-6 h-6 text-white right-2" />
+  <aside class="bg-base min-h-[93vh] relative pt-10" :class="open ? 'w-[5vw]' : 'w-[18vw]'">
+    <img src="/img/collapse.svg" alt="Collapse icon" class="invert absolute right-5" :class="open ? 'rotate-180' : ''" @click="open =!open" />
     <h2 class="text-white pl-5 font-medium mb-5">RECOMMENDED CHANELS</h2>
 
     <section class="flex flex-col text-white items relative">
       <div v-for="streamer in streamers" class="flex gap-3 my-3">
-        <NuxtImg :src="streamer.img" alt="Foto Perfil" width="40" height="40" class="border rounded-full border-base ml-5" />
+        <NuxtImg :src="streamer.img" alt="Foto Perfil" width="40" height="40"
+          class="border rounded-full border-base ml-5" />
         <div>
           <h3 class="font-medium">{{ streamer.name }}</h3>
           <p>{{ streamer.theme }}</p>
