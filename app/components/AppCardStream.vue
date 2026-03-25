e9822e8c154e424b959d1175c92c8477117eef9c
yesenia180405 <yes.ale1804@gmail.com>
2026-03-25 15:24:00 +0100

    chore(nuxt.config.ts): configure environment variables and nuxt settings

--- app/components/AppCardStream.vue
+++ app/components/AppCardStream.vue
@@ -17,10 +17,10 @@ const props = defineProps({
       <section class="flex gap-3 my-3">
         <img src="/img/streamer.jpg" alt="streamer" class="h-7 w-7 md:h-10 md:w-10 rounded-full ">
         <div class="flex flex-col gap-1 min-w-0">
-          <p class="font-semibold text-xs sm:text-sm lg:text-base ">{{ props.title }}</p>
+          <p class="font-semibold text-xs sm:text-sm lg:text-base truncate">{{ props.title }}</p>
           <p class="text-gray-300 text-xs sm:text-sm lg:text-base truncate">{{ props.user_name }}</p>
           <p class="text-gray-300 text-xs sm:text-sm lg:text-base truncate">{{ props.game_name }}</p>
-          <div class="flex gap-2 mt-1 flex-wrap">
+          <div class="flex gap-2 mt-1">
             <p class="bg-tags rounded-2xl px-2 text-xs text-gray-400 font-semibold">{{ props.language==="es" ? "Español" : props.language }}</p>
             <p class="bg-tags rounded-2xl px-2 text-xs text-gray-400 font-semibold truncate">DropsActivados</p>
           </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  css: ['~/assets/css/main.css'],
+  compatibilityDate: "2025-07-15",
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
+  runtimeConfig: {
+    twitchClientId: process.env.TWITCH_CLIENT_ID,
+    twitchClientSecret: process.env.TWITCH_CLIENT_SECRET,
+  },
+  
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/icon', '@pinia/nuxt', '@nuxt/eslint']
-})
+  modules: ["@nuxt/image", "@nuxt/icon", "@pinia/nuxt", "@nuxt/eslint"],
+});
