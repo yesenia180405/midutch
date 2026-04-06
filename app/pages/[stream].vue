7fe1e3669642d64f8774993e54a4a22033dbd41c
yesenia180405 <yes.ale1804@gmail.com>
2026-04-06 17:15:56 +0200

    feat([stream].vue): implement video and chat iframe for Twitch streams

--- app/pages/[stream].vue
+++ app/pages/[stream].vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
 const route = useRoute();
+const channel = route.params.stream; 
 </script>
 
 <template>
-  <section>{{ route.fullPath }}
+  <section class="flex h-screen">
+    <!-- Video -->
+    <div class="flex-1">
+      <iframe
+        :src="`https://player.twitch.tv/?channel=${channel}&parent=localhost&autoplay=true`"
+        width="100%"
+        height="100%"
+        allowfullscreen
+      />
+    </div>
+
+    <!-- Chat -->
+    <div class="w-80">
+      <iframe
+        :src="`https://www.twitch.tv/embed/${channel}/chat?parent=localhost&darkpopout`"
+        width="100%"
+        height="100%"
+      />
+    </div>
   </section>
 </template>
