27e45084fb690c83a26301f88c480616b3dd3814
yesenia180405 <yes.ale1804@gmail.com>
2026-04-06 17:29:31 +0200

    feat([stream].vue, index.vue): create loading state and refactor AsyncStreams component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,16 @@
+<script setup lang="ts">
+const AsyncStreams = defineAsyncComponent(() =>
+  import('@/components/AppStreams.vue')
+)
+</script>
+
 <template>
-    <section class="bg-base-bg pb-16">
-        <AppStreams :start-slice="0" :end-slice="4" />
-        <AppStreams :start-slice="8" :end-slice="12" />
-        <AppCategories />
-        <AppTheme />
-        <AppStreams :start-slice="16" :end-slice="20" />
-        <AppStreams :start-slice="24" :end-slice="28" />
-    </section>
+  <section class="bg-base-bg pb-16">
+    <AsyncStreams :start-slice="0" :end-slice="4" />
+    <AsyncStreams :start-slice="8" :end-slice="12" />
+    <AppCategories />
+    <AppTheme />
+    <AsyncStreams :start-slice="16" :end-slice="20" />
+    <AsyncStreams :start-slice="24" :end-slice="28" />
+  </section>
 </template>
